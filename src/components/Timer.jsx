e32dcc46178fa30b64bd73e01514abe61a1c0131
mayank1718 <mayank.vatas.2004@gmail.com>
2026-07-20 16:48:39 +0530

    Implement Pomodoro timer functionality with session management

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlassCard from "./GlassCard";
 
 const Timer = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [mode, setMode] = useState("work");
+  const [timeLeft, setTimeLeft] = useState(5);
+  const [completedSession, setCompletedSession] = useState(0);
+
+  const min = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const sec = (timeLeft % 60).toString().padStart(2, "0");
+
+  useEffect(() => {
+    if (isRunning) {
+      const timer = setInterval(() => {
+        if (timeLeft > 0) {
+          setTimeLeft((prev) => prev - 1);
+        } else {
+          setIsRunning(false);
+          setCompletedSession((prev) => prev + 1);
+          if (completedSession % 4 === 0) {
+            setTimeLeft(300)
+            setMode("Sort break")
+          } else {
+            setTimeLeft(900)
+            setMode("Long break")
+          }
+        }
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [isRunning, timeLeft, completedSession]);
+
   return (
     <GlassCard highlighted>
       <div className="flex items-start justify-between">
@@ -12,12 +43,16 @@ const Timer = () => {
           </p>
         </div>
         <div className="rounded-full bg-amber-500/15 px-3 py-1 text-sm font-medium text-amber-300">
-          Focus
+          {mode}
         </div>
       </div>
       <div className="mt-5 flex items-center justify-center">
-        <div className="flex h-32 w-32 items-center justify-center rounded-full border-xl border-sky-400/20 bg-gradient-to-br from-sky-700 to-violet-500 text-3xl font-semibold text-white shadow-lg shadow-sky-900/40">
-          25:00
+        <div
+          onClick={() => {
+            setIsRunning(!isRunning);
+          }}
+          className="flex h-32 w-32 items-center justify-center rounded-full border-xl border-sky-400/20 bg-gradient-to-br from-sky-700 to-violet-500 text-3xl font-semibold text-white shadow-lg shadow-sky-900/40">
+          {min} : {sec}
         </div>
       </div>
       <div className="mt-4 h-2 overflow-hidden rounded-full bg-slate-800">
