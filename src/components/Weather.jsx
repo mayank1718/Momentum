55d2fe523e3f0578161e3070b940ad7584ac99b9
mayank1718 <mayank.vatas.2004@gmail.com>
2026-07-22 16:21:32 +0530

    Implement weather fetching functionality with dynamic temperature display

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,27 @@
 import { Waves } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlassCard from "./GlassCard";
+import axios from "axios";
 
 const Weather = () => {
+  const [temperature, setTemperature] = useState(23);
+  const [city, setCity] = useState("");
+
+  function getTemperature() {
+    axios
+      .get(
+        "http://api.weatherstack.com/current?access_key=a9b1bc0374e4bdf35537710d7e0a0e87&query=Lucknow",
+      )
+      .then((res) => {
+        setTemperature(res.data.current.temperature);
+        setCity(res.data.location.name);
+      });
+  }
+
+  useEffect(() => {
+    getTemperature();
+  }, []);
+
   return (
     <GlassCard>
       <div className="flex items-center justify-between">
@@ -14,11 +33,41 @@ const Weather = () => {
       </div>
       <div className="mt-4 flex items-end justify-between">
         <div>
-          <p className="text-4xl font-semibold text-slate-50">18°</p>
-          <p className="mt-1 text-sm text-slate-400">San Francisco</p>
+          <p className="text-4xl font-semibold text-slate-50">
+            {" "}
+            {temperature} &deg;c{" "}
+          </p>
+          <p className="mt-1 text-sm text-slate-400"> {city} </p>
         </div>
-        <div className="rounded-2xl bg-sky-500/15 px-3 py-2 text-sm font-medium text-sky-300">
-          Light jacket
+        <div
+          className={
+            temperature <= 0
+              ? " bg-[#063b4d] text-[#86ffe9] rounded-2xl px-3 py-2 text-sm font-medium"
+              : temperature > 0 && temperature <= 10
+                ? "rounded-2xl bg-[#1E3A8A] text-[#BFDBFE] px-3 py-2 text-sm font-medium"
+                : temperature > 10 && temperature <= 18
+                  ? "rounded-2xl bg-[#1E40AF] text-[#DBEAFE] px-3 py-2 text-sm font-medium"
+                  : temperature > 18 && temperature <= 25
+                    ? "rounded-2xl bg-[#14532D] text-[#BBF7D0] px-3 py-2 text-sm font-medium"
+                    : temperature > 25 && temperature <= 30
+                      ? "rounded-2xl bg-[#78350F] text-[#FDE68A] px-3 py-2 text-sm font-medium"
+                      : temperature > 30 && temperature <= 35
+                        ? "rounded-2xl bg-[#7C2D12] text-[#FED7AA] px-3 py-2 text-sm font-medium"
+                        : "rounded-2xl bg-[#991B1B] text-[#FECACA] px-3 py-2 text-sm font-medium"
+          }>
+          {temperature <= 0
+            ? "🥶 Freezing"
+            : temperature > 0 && temperature <= 10
+              ? "🧥 Warm Jacket"
+              : temperature > 10 && temperature <= 18
+                ? "🧥 Light Jacket"
+                : temperature > 18 && temperature <= 25
+                  ? "🌤 Pleasant"
+                  : temperature > 25 && temperature <= 30
+                    ? "☀️ Stay Hydrated"
+                    : temperature > 30 && temperature <= 35
+                      ? "🔥 Hot"
+                      : "🚨 Extreme Heat"}
         </div>
       </div>
     </GlassCard>
